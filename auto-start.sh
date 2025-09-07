f922882399e917d1688e3929352bad6c09ed9041
SpartanBatman <84638207+SpartanBatman@users.noreply.github.com>
2025-09-07 08:15:24 +0100

    Update auto-start.sh
    Added USCM_Logo to the boot sequence to give system more time to stabilize, and remove signs of the terminal making it more immersive.

--- auto-start.sh
+++ auto-start.sh
@@ -1,8 +1,18 @@
-
 #!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
+# auto-start.sh
+
+sleep 2
+
+# Show logo, keep process running in background and hides the console from the screen.
+sudo fbi -T 1 -d /dev/fb1 -noverbose -a /home/pi/AliensMotionTracker/resources/startup/USCM_Logo.png &
+FBI_PID=$!
 
 sleep 10
+
 fbcp &
+
+# Kill fbi just before launching tracker so console doesn’t pop up
+sudo kill $FBI_PID
+
+# Launch tracker
 sudo SDL_AUDIODRIVER=alsa AUDIODEV=hw:0,0 SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 python3 /home/pi/AliensMotionTracker/main.py
